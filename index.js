2fd74608a0983b18b7ceef2461ca0a9a2342bcd9
Guilherme Maffessoni <maffessonig@gmail.com>
2025-04-01 21:10:55 -0300

    Implementada a conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('biblioteca2025', 'postgres', 'postgres', {
+    host: 'localhost',
+    port: 5432,
+    dialect:'postgres', 
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+  });
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 const app = express();
 app.use(express.json());
