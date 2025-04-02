16b576925cde60d13e6bc1e6b61ca75bf72fd093
Guilherme Maffessoni <maffessonig@gmail.com>
2025-04-01 21:31:47 -0300

    Mapeamento da model Editora

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { Sequelize } from "sequelize";
+import { Sequelize, DataTypes } from "sequelize";
 
+
+//configuração da conexão com o banco de dados
 const sequelize = new Sequelize('biblioteca2025', 'postgres', 'postgres', {
     host: 'localhost',
     port: 5432,
@@ -10,6 +12,32 @@ const sequelize = new Sequelize('biblioteca2025', 'postgres', 'postgres', {
         freezeTableName: true
     }
   });
+
+  //mapeamento da model Editora
+  const Editora = sequelize.define(
+    'editora',
+    {
+      ideditora: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nomeeditora: {
+        type: DataTypes.STRING(60),
+        allowNull: false,
+      },
+      cnpj: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+      },
+      endereco: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      }
+    }
+  );
+  
 
 try {
     await sequelize.authenticate();
