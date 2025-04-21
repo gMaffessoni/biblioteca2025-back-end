10825b716d59f77a38ec5718f853c9ce79f39f58
Guilherme Maffessoni <maffessonig@gmail.com>
2025-04-21 18:44:03 -0300

    Incluindo método de devolução

---
+++ EmprestimoModel.js
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+import banco from "../banco.js"
+
+//mapeamento da model Emprestimo
+export default banco.define(
+    'emprestimo',
+    {
+      idemprestimo: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      idlivro: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      idusuario: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      emprestimo: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+      },
+      vencimento: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+      },
+      devolucao: {
+        type: DataTypes.DATEONLY,
+        allowNull: true
+      },
+      observacao: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      }
+    }
+  );
