42c5b99304eed190dcbf5bb5f472560b6cd81c47
Guilherme Maffessoni <maffessonig@gmail.com>
2025-06-03 19:31:36 -0300

    Questão 1 - Editoras

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Menu from './componentes/Menu';
+import FormCategoria from './paginas/FormCategoria';
+import Home from './paginas/Home';
+import ListaCategoria from './paginas/ListaCategoria';
+import ListaAutor from './paginas/ListaAutor';
+import FormAutor from './paginas/FormAutor';
+import ListaEditora from './paginas/ListaEditora';
+import FormEditora from './paginas/FormEditora';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Menu/>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/listacategoria' element={<ListaCategoria/>}/>
+            <Route path='/cadastrocategoria' element={<FormCategoria/>}/>
+            <Route path='/cadastrocategoria/:id' element={<FormCategoria/>}/>
+
+            <Route path='/listaautor' element={<ListaAutor/>}/>
+            <Route path='/cadastroautor' element={<FormAutor/>}/>
+            <Route path='/cadastroautor/:id' element={<FormAutor/>}/>
+
+            <Route path='/listaeditora' element={<ListaEditora/>}/>
+            <Route path='/cadastroeditora' element={<FormEditora/>}/>
+            <Route path='/cadastroeditora/:id' element={<FormEditora/>}/>
+
+            <Route path='*' element={<Home/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
